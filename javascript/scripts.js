ad84b320aac75dae0263bfdb39a54bb618da9247
Luis Felipe Elizeu <l_felipelizeu@hotmail.com>
2021-07-23 21:31:54 -0300

    Implementada a funcionalidade de clicar em uma opção e seleciona-lá e descelecionar outra, botao tabmém atualiza quando forem selecionadas as 3 opções, adicionada a pasta assets para colocar outras opções de comidas/bebidas/sobremesas

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,13 +1,100 @@
-function selecionar() {
+let prato_nome;
+let prato_preco;
+
+let bebida_nome;
+let bebida_preco;
+
+let sobremesa_nome;
+let sobremesa_preco;
+
+function atualizarBotao() {
+    const botao_desabilitado = document.querySelector(".desabilitado");
+    const botao_habilitado = document.querySelector(".habilitado");
+
+    if ( prato_nome !== undefined && bebida_nome !== undefined && sobremesa_nome !== undefined ) {
+        botao_desabilitado.classList.add("oculto");
+        botao_habilitado.classList.remove("oculto");
+    }
+}
+
+function selecionarPrato(opcao) {
+    const selecionado = document.querySelector(".pratos .selecionado");
+    let check = document.querySelector(".pratos .selecionado .check");
+
+    if ( selecionado !== null )
+    {
+        selecionado.classList.remove("selecionado");
+        check.classList.add("oculto");
+    }
+    
+    opcao.classList.add("selecionado");
+    check = document.querySelector(".pratos .selecionado .check");
+    check.classList.remove("oculto");
+
+    const nome = document.querySelector(".pratos .selecionado .nome");
+    const preco = document.querySelector(".pratos .selecionado .preco");
+
+    prato_nome = nome.innerHTML;
+    prato_preco = Number(preco.innerHTML);
+
+    atualizarBotao();
+}
+
+function selecionarBebida(opcao) {
+    const selecionado = document.querySelector(".bebidas .selecionado");
+    let check = document.querySelector(".bebidas .selecionado .check");
+
+    if ( selecionado !== null )
+    {
+        selecionado.classList.remove("selecionado");
+        check.classList.add("oculto");
+    }
+    
+    opcao.classList.add("selecionado");
+    check = document.querySelector(".bebidas .selecionado .check");
+    check.classList.remove("oculto");
+
+    const nome = document.querySelector(".bebidas .selecionado .nome");
+    const preco = document.querySelector(".bebidas .selecionado .preco");
+
+    bebida_nome = nome.innerHTML;
+    bebida_preco = Number(preco.innerHTML);
+
+    atualizarBotao();
+}
+
+function selecionarSobremesa(opcao) {
+    const selecionado = document.querySelector(".sobremesas .selecionado");
+    let check = document.querySelector(".sobremesas .selecionado .check");
+
+    if ( selecionado !== null )
+    {
+        selecionado.classList.remove("selecionado");
+        check.classList.add("oculto");
+    }
+    
+    opcao.classList.add("selecionado");
+    check = document.querySelector(".sobremesas .selecionado .check");
+    check.classList.remove("oculto");
+
+    const nome = document.querySelector(".sobremesas .selecionado .nome");
+    const preco = document.querySelector(".sobremesas .selecionado .preco");
+
+    sobremesa_nome = nome.innerHTML;
+    sobremesa_preco = Number(preco.innerHTML);
+
+    atualizarBotao();
 }
 
 function enviarMensagem() {
+    const preco = prato_preco + bebida_preco + sobremesa_preco;
+
     const numero = "5512996421592";
     const pedido = "Olá, gostaria de fazer o pedido:\n";
-    const prato = "- Prato: Frango Yin Yang\n";
-    const bebida = "- Bebida: Coquinha Gelada\n";
-    const sobremesa = "- Sobramesa: Pudim\n";
-    const total = "Total: R$ 27.70";
+    const prato = "- Prato: " + prato_nome;
+    const bebida = "\n- Bebida: " + bebida_nome;
+    const sobremesa = "\n- Sobremesa: " + sobremesa_nome;
+    const total = "\nTotal: R$ " + preco;
 
     const mensagem = pedido + prato + bebida + sobremesa + total;
 
